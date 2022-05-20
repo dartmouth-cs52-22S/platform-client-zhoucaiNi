c122269ba6908a0fb996ce6492d19a4368a035ff
Zhoucai <nizhoucai@gmail.com>
2022-05-20 16:06:40 -0400

    api change again

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // const ROOT_URL = 'http://localhost:9090/api';
 // const ROOT_URL = 'https://platform.cs52.me/api';
-const ROOT_URL = 'https://zhoucai-api-with-auth.onrender.com';
+const ROOT_URL = 'https://zhoucai-api-with-auth.onrender.com/api';
 const API_KEY = '?key=ZNI';
 
 // keys for actiontypes
